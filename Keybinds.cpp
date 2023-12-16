a4a18ea04ac3d1018f53269e794f4f8116791851
ToufikZer <amrouche.tarik@gmail.com>
2023-12-16 21:49:07 +0100

    ajout de la page keybind

--- Keybinds.cpp
+++ Keybinds.cpp
@@ -9,24 +9,31 @@ Keybinds::Keybinds(sf::RenderWindow& window):
         std::cerr << "Erreur lors du chargement de la police" << std::endl;
         std::exit(-1);
         }
-        if (!backgroundTexture.loadFromFile("texture/texture_decor/Titlescreen.jpg")) {
+        if (!backgroundTexture.loadFromFile("texture/texture_decor/keybinds.png")) {
         // Gestion de l'erreur si le chargement échoue
         std::cerr << "Erreur lors du chargement de l'image de fond" << std::endl;
         std::exit(-1);
         }
+
+        backgroundSprite.setTexture(backgroundTexture);
+        backgroundSprite.setPosition(0,0);
+
         text.setFont(font);
-        text.setString("Press A to interact \n Press R to change the map");
+        text.setString("Press A to interact \nPress R to change the map");
         text.setCharacterSize(44);
-        text.setPosition(window.getSize().x*0.46, 0.44*window.getSize().y);
+        text.setPosition(window.getSize().x*0.5 - text.getGlobalBounds().width * 0.5, 0.15*window.getSize().y);
         text.setFillColor(sf::Color::Red);
     }
 
 void Keybinds::handleEvent(sf::Event& event, sf::RenderWindow& window){
-    
+
 }
 void Keybinds::update(sf::Time deltaTime, sf::RenderWindow& window){
 
 }
 void Keybinds::draw(sf::RenderWindow& window){
-
+    window.clear();
+    window.draw(backgroundSprite);
+    window.draw(text);
+    window.display();
 }
