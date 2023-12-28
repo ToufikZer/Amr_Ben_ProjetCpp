ac06770bcc1a83be0317d21b891d24c5a852521e
ToufikZer <amrouche.tarik@gmail.com>
2023-12-28 23:59:12 +0100

    ajout inventaire + test avec clé et ca marche

--- Item.hpp
+++ Item.hpp
@@ -6,15 +6,7 @@
 class Item {
 public:
     Item(const std::string& name, const std::string& description, int damage, const std::string& texturePath);
-    Item& operator=(const Item& other) {
-    if (this != &other) {
-        name = other.name;
-        description = other.description;
-        damage = other.damage;
-        texture = other.texture;
-    }
-    return *this;
-}
+
     const std::string& getName() const;
     const std::string& getDescription() const;
     int getDamage() const;
